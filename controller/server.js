0b203f10b1204e785916bcf814e9332f27d9a5d7
fikri <erhafikri@gmail.com>
2022-07-24 17:29:11 +0700

    membuat server.js

---
+++ controller/server.js
@@ -0,0 +1,12 @@
+const bodyParser = require('body-parser');
+const express = require('express'); 
+const app = express();
+
+// membuat server
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+app.listen(3000, () => {
+    console.log('Server berjalan di port 3000');
+});
